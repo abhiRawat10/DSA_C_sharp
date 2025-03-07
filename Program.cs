160d2e48a2a8d99c9fc2864a45bd5b391850e8bb
abhirawat10 <abhi10rawat2002@gmail.com>
2025-03-07 14:47:31 +0530

    initial commmit

--- Program.cs
+++ Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Queue<int> q = new Queue<int>();
+            while (true)
+            {
+                Console.WriteLine("Press 1 to Add  element in queue");
+                Console.WriteLine("Press 2 to Remove element in queue");
+                Console.WriteLine("Press 3 to print the queue");
+                Console.WriteLine("Press 4 to getSize");
+                Console.WriteLine("press 5 to exit");
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        Console.WriteLine("Enter element to add");
+                        int n=Convert.ToInt32(Console.ReadLine());
+                        q.Add(n);
+                        break;
+
+                    case "2":
+                        q.Remove();
+                        break;
+
+                    case "3":
+                        q.PrintQueue();
+                        break;
+
+                    case "4":
+                        Console.WriteLine(q.getSize());
+                        break;
+
+                    case "5":
+                        return;
+
+                        
+                }
+            }
         }
     }
 }
---
+++ Queue.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_DSA
+{
+    internal class Queue<T>
+    {
+        LinkedList<T> queue;
+        public Queue()
+        {
+
+            queue = new LinkedList<T>();
+
+        }
+
+        public void Add(T n)
+        {
+            queue.AddFirst(n);
+        }
+
+        public T Remove() 
+        {
+            if (queue.Count == 0) return default(T);
+            LinkedListNode<T> t = queue.Last;
+            queue.RemoveLast();
+            return t.Value;
+        }
+
+        public int getSize()
+        {
+            return queue.Count;
+        }
+
+        public void PrintQueue()
+        {
+            foreach (T n in queue)
+            {
+                Console.WriteLine($"{n} ");
+            }
+        }
+
+    }
+
+}
